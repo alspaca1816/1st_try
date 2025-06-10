5aa081f4396b0560a84af0a37c0d3fa23eaa8dac
ndnrb <brmendezo@gmail.com>
2025-06-09 19:56:07 -0500

    Add files via upload

--- projFX/src/vistas/comprador/RegistroController.java
+++ projFX/src/vistas/comprador/RegistroController.java
@@ -1,13 +1,20 @@
 
 package vistas.comprador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class RegistroController implements Initializable {
@@ -33,6 +40,47 @@ public class RegistroController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        ingresarbtn.setOnAction(this::handleIngreso);
+        vendedorbtn.setOnAction(this::handleVendedor);
+    }   
+    
+    private void handleVendedor(ActionEvent event){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/vendedor/LoginVendedor.fxml"));
+            Parent root = loader.load();
+            
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Ingreso como Vendedor");
+            stage.show();
+            
+            Node source = (Node) event.getSource();
+            Stage currentStage = (Stage) source.getScene().getWindow();
+            currentStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    private void handleIngreso(ActionEvent event1){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/projfx/vista.fxml"));
+            Parent root = loader.load();
+            
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Ingreso como Usuario");
+            stage.show();
+            Node source = (Node) event1.getSource();
+            Stage currentStage = (Stage) source.getScene().getWindow();
+            currentStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
     
 }
