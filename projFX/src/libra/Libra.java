31408717401f66497ba3514e1bee59c2b746d163
ndnrb <brmendezo@gmail.com>
2025-06-09 19:58:35 -0500

    Libra.java

--- projFX/src/libra/Libra.java
+++ projFX/src/libra/Libra.java
@@ -1,8 +1,27 @@
 package libra;
 
-public class Libra {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Libra extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/projfx/Vista.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Inicio de Sesión");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        
+        launch(args);
     }
 }
